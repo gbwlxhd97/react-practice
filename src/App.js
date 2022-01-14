f696feadb26bb8d80f6399ee96bb50276e28db38
gbwlxhd97 <gbwlxhd97@naver.com>
2022-01-14 23:21:17 +0900

    props 넘겨주기 && 컴포넌트 상속

--- src/App.js
+++ src/App.js
@@ -1,7 +1,24 @@
 import styled from "styled-components";
 
+const Father = styled.div`
+  display: flex;
+`;
+const Box = styled.div`
+  background-color: ${(props) => props.bgColor};
+  width: 100px;
+  height: 100px;
+`;
+const Circle = styled(Box)`
+  border-radius: 50px;
+`;
+
 function App() {
-  return null;
+  return (
+    <Father>
+      <Box bgColor="teal" />
+      <Circle bgColor="tomato" />
+    </Father>
+  );
 }
 
 export default App;
