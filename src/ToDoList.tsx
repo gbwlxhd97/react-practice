951067daf8eb6a01f29b51cb570d8774c6b8a36c
gbwlxhd97 <gbwlxhd97@naver.com>
2022-01-23 23:07:29 +0900

    form hooks 유효성검사 체크

--- src/ToDoList.tsx
+++ src/ToDoList.tsx
@@ -74,7 +74,13 @@ function ToDoList() {
             />
             <span>{errors?.email?.message}</span>
             <input
-                {...register("firstName", { required: "write here" })}
+                {...register("firstName", {
+                    required: "write here",
+                    validate: {
+                    noCute: (value) =>
+                        value.includes("cute") ? "no cute allowed" : true
+                    },
+                })}
                 placeholder="First Name"
             />
             <span>{errors?.firstName?.message}</span>
