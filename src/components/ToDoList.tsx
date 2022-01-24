0ee392e30f821906a427571444a4cd6f0d11d797
gbwlxhd97 <gbwlxhd97@naver.com>
2022-01-24 23:23:25 +0900

    투두 리스트 slice state 리 랜더링

---
+++ src/components/ToDoList.tsx
@@ -0,0 +1,24 @@
+import { useRecoilValue } from "recoil";
+import { toDoState } from "../atoms";
+import CreateToDo from "./CreateToDo";
+import ToDo from "./ToDo";
+
+function ToDoList() {
+    const toDos = useRecoilValue(toDoState);
+    console.log(toDos);
+    
+    return (
+        <div>
+        <h1>To Dos</h1>
+        <hr />
+        <CreateToDo />
+        <ul>
+            {toDos.map((toDo) => (
+            <ToDo key={toDo.id} {...toDo} />
+            ))}
+        </ul>
+        </div>
+    );
+}
+
+export default ToDoList;
