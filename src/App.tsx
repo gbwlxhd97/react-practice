ffad259e6025f8d8e599190572a3c919aefe8b4f
gbwlxhd97 <gbwlxhd97@naver.com>
2022-01-28 23:31:50 +0900

    드래고 인덱스 atom 저장

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,12 @@
-import { DragDropContext, Droppable, Draggable } from "react-beautiful-dnd";
+import {
+  DragDropContext,
+  Droppable,
+  Draggable,
+  DropResult,
+} from "react-beautiful-dnd";
+import { useRecoilState } from "recoil";
 import styled from "styled-components";
+import { toDoState } from "./atoms";
 
 const Wrapper = styled.div`
   display: flex;
@@ -32,10 +39,25 @@ const Card = styled.div`
   background-color: ${(props) => props.theme.cardColor};
 `;
 
-const toDos = ["a", "b", "c", "d", "e", "f"];
-
 function App() {
-  const onDragEnd = () => {};
+  const [toDos, setToDos] = useRecoilState(toDoState);
+  const onDragEnd = ({ draggableId, destination, source }: DropResult) => {
+    if (!destination) return;
+    setToDos((oldToDos) => {
+      const toDosCopy = [...oldToDos];
+      // 1) Delete item on source.index
+      console.log("Delete item on", source.index);
+      console.log(toDosCopy);
+      toDosCopy.splice(source.index, 1);
+      console.log("Deleted item");
+      console.log(toDosCopy);
+      // 2) Put back the item on the destination.index
+      console.log("Put back", draggableId, "on ", destination.index);
+      toDosCopy.splice(destination?.index, 0, draggableId);
+      console.log(toDosCopy);
+      return toDosCopy;
+    });
+  };
   return (
     <DragDropContext onDragEnd={onDragEnd}>
       <Wrapper>
@@ -44,7 +66,7 @@ function App() {
             {(magic) => (
               <Board ref={magic.innerRef} {...magic.droppableProps}>
                 {toDos.map((toDo, index) => (
-                  <Draggable draggableId={toDo} index={index}>
+                  <Draggable key={toDo} draggableId={toDo} index={index}>
                     {(magic) => (
                       <Card
                         ref={magic.innerRef}
--- src/atoms.ts
+++ src/atoms.ts
@@ -1,32 +1,6 @@
 import { atom, selector } from "recoil";
 
-export interface IToDo {
-    text: string;
-    id: number;
-    category: "TO_DO" | "DOING" | "DONE";
-}
-
-export enum Categories {
-    "TO_DO" = "TO_DO",
-    "DOING" = "DOING",
-    "DONE" = "DONE",
-}
-
-export const toDoState = atom<IToDo[]>({
+export const toDoState = atom({
     key: "toDo",
-    default: [],
-});
-
-export const categoryState = atom({
-    key: "category",
-    default: Categories.TO_DO,
-});
-
-export const toDoSelector = selector({
-    key: "toDoSelector",
-    get: ({get}) => {
-        const toDos = get(toDoState)
-        const category = get(categoryState);
-        return toDos.filter((toDo) => toDo.category === category);
-    },
+    default: ["a", "b", "c", "d", "e", "f"],
 });
