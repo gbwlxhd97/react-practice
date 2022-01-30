716c8cb8c4658647f859fbdbe1ae88ac5f9b3190
gbwlxhd97 <gbwlxhd97@naver.com>
2022-01-30 12:57:34 +0900

    카드 style 및 카드끼리 이동

--- src/Components/Board.tsx
+++ src/Components/Board.tsx
@@ -4,11 +4,13 @@ import DragabbleCard from "./DragabbleCard";
 
 const Wrapper = styled.div`
     width: 300px;
-    padding: 20px 10px;
     padding-top: 10px;
     background-color: ${(props) => props.theme.boardColor};
     border-radius: 5px;
     min-height: 300px;
+    display: flex;
+    flex-direction: column;
+    overflow: hidden;
 `;
 
 const Title = styled.h2`
@@ -16,6 +18,22 @@ const Title = styled.h2`
     font-weight: 600;
     margin-bottom: 10px;
     font-size: 18px;
+`;
+
+interface IAreaProps {
+    isDraggingFromThis: boolean;
+    isDraggingOver: boolean;
+}
+const Area = styled.div<IAreaProps>`
+    background-color: ${(props) =>
+    props.isDraggingOver
+        ? "#dfe6e9"
+        : props.isDraggingFromThis
+        ? "#b2bec3"
+        : "transparent"};
+    flex-grow: 1;
+    transition: background-color 0.3s ease-in-out;
+    padding: 20px;
 `;
 
 interface IBoardProps {
@@ -28,13 +46,18 @@ function Board({ toDos, boardId }: IBoardProps) {
         <Wrapper>
         <Title>{boardId}</Title>
         <Droppable droppableId={boardId}>
-            {(magic) => (
-            <div ref={magic.innerRef} {...magic.droppableProps}>
+            {(magic,info) => (
+            <Area
+                isDraggingOver={info.isDraggingOver}
+                isDraggingFromThis={Boolean(info.draggingFromThisWith)}
+                ref={magic.innerRef}
+                {...magic.droppableProps}
+            >
                 {toDos.map((toDo, index) => (
                 <DragabbleCard key={toDo} index={index} toDo={toDo} />
                 ))}
                 {magic.placeholder}
-            </div>
+            </Area>
             )}
         </Droppable>
         </Wrapper>
