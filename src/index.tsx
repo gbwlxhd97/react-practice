fc874f397a2e2dfeefa9f69ecc123341164fccc9
gbwlxhd97 <gbwlxhd97@naver.com>
2022-01-31 17:06:36 +0900

    애니메이션 motion 라이브러리 설치 및 craco 셋팅..

--- src/index.tsx
+++ src/index.tsx
@@ -62,7 +62,7 @@ table {
 body {
   font-weight: 300;
   font-family: 'Source Sans Pro', sans-serif;
-  background-color:${(props) => props.theme.bgColor};
+  background:linear-gradient(135deg,#e09,#d0e);
   color:black;
   line-height: 1.2;
 }
